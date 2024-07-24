8a81aa6db6c08680b3da4b4b91c543a14e6dc99e
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2024-07-24 19:42:47 -0300

    Creating the base context

---
+++ src/Accountbank.Infra/Accountbank.Infra.Data/Context/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Accountbank.Infra.Data.Context;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+{
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		throw new NotImplementedException();
+	}
+}
