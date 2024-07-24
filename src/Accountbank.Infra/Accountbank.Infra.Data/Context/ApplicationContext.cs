db72acc493ecbe6e9b3c0b6a4f231ef7952127ca
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2024-07-24 19:54:38 -0300

    Transaction entity mapping configuration

--- src/Accountbank.Infra/Accountbank.Infra.Data/Context/ApplicationContext.cs
+++ src/Accountbank.Infra/Accountbank.Infra.Data/Context/ApplicationContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Accountbank.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Accountbank.Infra.Data.Context;
 
 public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
 {
+	public DbSet<Transaction> Transactions { get; set; }
+
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		throw new NotImplementedException();
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
 	}
 }
---
+++ src/Accountbank.Infra/Accountbank.Infra.Data/Context/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Accountbank.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accountbank.Infra.Data.Context.Configurations;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+	public void Configure(EntityTypeBuilder<Transaction> builder)
+	{
+		builder.ToTable("Transactions");
+		builder.HasKey(x => x.Id);
+		builder.Property(x => x.Date).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd().IsRequired();
+		builder.Property(x => x.TransactionType).HasConversion<int>().IsRequired();
+		builder.Property(x => x.Amount).IsRequired();
+	}
+}
