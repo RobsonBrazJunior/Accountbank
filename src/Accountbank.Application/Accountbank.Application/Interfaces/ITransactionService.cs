57af744c39abaf7501590127db8fdf4dcf4805dd
RobsonBrazJunior <robsonbrazjunior@gmail.com>
2024-07-25 02:07:28 -0300

    Creation of services

---
+++ src/Accountbank.Application/Accountbank.Application/Interfaces/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using Accountbank.Domain.Models;
+
+namespace Accountbank.Application.Interfaces;
+
+public interface ITransactionService : IDisposable
+{
+	Transaction GetById(Guid id);
+	IEnumerable<Transaction> GetAll();
+	void Add(Transaction transaction);
+	void Update(Transaction transaction);
+	void Remove(Transaction transaction);
+}
---
+++ src/Accountbank.Application/Accountbank.Application/Services/TransactionService.cs
@@ -0,0 +1,43 @@
+﻿using Accountbank.Application.Interfaces;
+using Accountbank.Domain.Models;
+using Accountbank.Infra.Data.Repository.Interfaces;
+
+namespace Accountbank.Application.Services;
+
+public class TransactionService(IUnitOfWork unitOfWork) : ITransactionService
+{
+	private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+	public Transaction GetById(Guid id)
+	{
+		return _unitOfWork.TransactionRespository.GetById(id);
+	}
+
+	public IEnumerable<Transaction> GetAll()
+	{
+		return _unitOfWork.TransactionRespository.GetAll();
+	}
+
+	public void Add(Transaction transaction)
+	{
+		_unitOfWork.TransactionRespository.Add(transaction);
+		_unitOfWork.Save();
+	}
+
+	public void Update(Transaction transaction)
+	{
+		_unitOfWork.TransactionRespository.Update(transaction);
+		_unitOfWork.Save();
+	}
+
+	public void Remove(Transaction transaction)
+	{
+		_unitOfWork.TransactionRespository.Remove(transaction);
+		_unitOfWork.Save();
+	}
+
+	public void Dispose()
+	{
+		GC.SuppressFinalize(this);
+	}
+}
